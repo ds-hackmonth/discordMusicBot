c2434b41c5c0c6e051ca00d0510e395d49052da6
Shivam Patel <yodashiv@gmail.com>
2020-10-10 18:16:29 -0700

    Obtaining client access token

--- spotifyExtractor.py
+++ spotifyExtractor.py
@@ -1,11 +1,34 @@
 import os
 import sys
+import spotipy
+import spotipy.oauth2 as oauth2
 
 CLI_ID = "c031db81e9e841c382309dac036c36ae"
-CLI_SEC = os.environ.get("spotifyDiscordBotSecret")
-if CLI_SEC is None:
+CLI_KEY = os.environ.get("spotifyDiscordBotSecret")
+if CLI_KEY is None:
     print("You have not added the spotify Client secret as an environment variable. Exiting.")
     sys.exit()
+
+def get_token():
+    """ Returns an access token that is to be sent to the spotify Web API upon every request."""
+    credentials = oauth2.SpotifyClientCredentials(
+        client_id = CLI_ID, 
+        client_secret = CLI_KEY)
+    token = credentials.get_access_token()
+    return token 
+
+def get_songs_from_album(playlist_url):
+    """ Given an ALBUM_URL, this function will query the spotify API (via the spotipy library) 
+    and return a list of all the songs present in that album"""
+    
+    # First, we must tell the spotify API who we are
+    token = get_token()
+    spotify = spotipy.Spotify(auth=token)
+    playlist = spotipy.playlist("2TrrC2ZsH9FcMPAtP0KP4b")
+    print(playlist)
+
+get_songs_from_album("")
+
 
 
 
